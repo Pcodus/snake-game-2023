3524f5c115a896421087bf523822a1f7cd9eb6ab
KIM JUNHEE <136769810+kong-juni@users.noreply.github.com>
2023-06-19 00:44:34 +0900

    victory / level up

--- Main.cpp
+++ Main.cpp
@@ -17,7 +17,8 @@ int last_dir = 5;
 int cur_dir = KEY_RIGHT;
 bool gameover;
 int maxsize = 3;
-int level = 1;
+int level = 0;
+bool victory[4] = {false,false,false,false};
 
 class G {
 public:
@@ -36,18 +37,18 @@ public:
 
 vector<G> growitems;
 vector<P> poisonitems;
-int itempoint[2] = {0,0};
+int GamePoint[3] = {0,0,0};
 vector<Gatepair> gatelist;
 
 
-int MoveUP(int map[][45], vector<vector<int> >& snake) {
+int MoveUP(int map[][24][45], vector<vector<int> >& snake) {
     vector<int> tmp;
     vector<int> head = snake[0];
 
     snake[0][0]--;
     // growth, poison, hit body
-    if(map[snake[0][0]][snake[0][1]] == 5) {
-        itempoint[0]++;
+    if(map[level][snake[0][0]][snake[0][1]] == 5) {
+        GamePoint[0]++;
         snake.insert(snake.begin(), {snake[0][0],snake[0][1]});
         snake[1][0]++;
         for(vector<G>::iterator it = growitems.begin(); it != growitems.end(); it++) {
@@ -57,8 +58,8 @@ int MoveUP(int map[][45], vector<vector<int> >& snake) {
             }
         }
     }
-    else if(map[snake[0][0]][snake[0][1]] == 6) {
-        itempoint[1]++;
+    else if(map[level][snake[0][0]][snake[0][1]] == 6) {
+        GamePoint[1]++;
         snake.pop_back();
         for(int i = 1; i < snake.size(); i++) {
             tmp = snake[i];
@@ -72,7 +73,7 @@ int MoveUP(int map[][45], vector<vector<int> >& snake) {
             }
         }
     }
-    else if(map[snake[0][0]][snake[0][1]] == 4) {
+    else if(map[level][snake[0][0]][snake[0][1]] == 4) {
         return 9999;
     }
     for(int i = 1; i < snake.size(); i++) {
@@ -83,13 +84,13 @@ int MoveUP(int map[][45], vector<vector<int> >& snake) {
     return KEY_UP;
 }
 
-int MoveDOWN(int map[][45], vector<vector<int> >& snake) {
+int MoveDOWN(int map[][24][45], vector<vector<int> >& snake) {
     vector<int> tmp;
     vector<int> head = snake[0];
 
     snake[0][0]++;
-    if(map[snake[0][0]][snake[0][1]] == 5) {
-        itempoint[0]++;
+    if(map[level][snake[0][0]][snake[0][1]] == 5) {
+        GamePoint[0]++;
         snake.insert(snake.begin(), {snake[0][0],snake[0][1]});
         snake[1][0]--;
         for(vector<G>::iterator it = growitems.begin(); it != growitems.end(); it++) {
@@ -104,8 +105,8 @@ int MoveDOWN(int map[][45], vector<vector<int> >& snake) {
             }
         }
         
-    } else if(map[snake[0][0]][snake[0][1]] == 6) {
-        itempoint[1]++;
+    } else if(map[level][snake[0][0]][snake[0][1]] == 6) {
+        GamePoint[1]++;
         snake.pop_back();
         for(int i = 1; i < snake.size(); i++) {
             tmp = snake[i];
@@ -119,7 +120,7 @@ int MoveDOWN(int map[][45], vector<vector<int> >& snake) {
             }
         }
     } 
-    else if(map[snake[0][0]][snake[0][1]] == 4) {
+    else if(map[level][snake[0][0]][snake[0][1]] == 4) {
         return 9999;
     }
     for(int i = 1; i < snake.size(); i++) {
@@ -130,13 +131,13 @@ int MoveDOWN(int map[][45], vector<vector<int> >& snake) {
     return KEY_DOWN;
 }
 
-int MoveLEFT(int map[][45], vector<vector<int> >& snake) {
+int MoveLEFT(int map[][24][45], vector<vector<int> >& snake) {
     vector<int> tmp;
     vector<int> head = snake[0];
 
     snake[0][1]--;
-    if(map[snake[0][0]][snake[0][1]] == 5) {
-        itempoint[0]++;
+    if(map[level][snake[0][0]][snake[0][1]] == 5) {
+        GamePoint[0]++;
         snake.insert(snake.begin(), {snake[0][0],snake[0][1]});
         snake[1][1]++;
         for(vector<G>::iterator it = growitems.begin(); it != growitems.end(); it++) {
@@ -146,8 +147,8 @@ int MoveLEFT(int map[][45], vector<vector<int> >& snake) {
             }
         }
         
-    } else if(map[snake[0][0]][snake[0][1]] == 6) {
-        itempoint[1]++;
+    } else if(map[level][snake[0][0]][snake[0][1]] == 6) {
+        GamePoint[1]++;
         snake.pop_back();
         for(int i = 1; i < snake.size(); i++) {
             tmp = snake[i];
@@ -161,7 +162,7 @@ int MoveLEFT(int map[][45], vector<vector<int> >& snake) {
             }
         }
     } 
-    else if(map[snake[0][0]][snake[0][1]] == 4) {
+    else if(map[level][snake[0][0]][snake[0][1]] == 4) {
         return 9999;
     }
     for(int i = 1; i < snake.size(); i++) {
@@ -172,13 +173,13 @@ int MoveLEFT(int map[][45], vector<vector<int> >& snake) {
     return KEY_LEFT;
 }
 
-int MoveRIGHT(int map[][45], vector<vector<int> >& snake) {
+int MoveRIGHT(int map[][24][45], vector<vector<int> >& snake) {
     vector<int> tmp;
     vector<int> head = snake[0];
 
     snake[0][1]++;
-    if(map[snake[0][0]][snake[0][1]] == 5) {
-        itempoint[0]++;
+    if(map[level][snake[0][0]][snake[0][1]] == 5) {
+        GamePoint[0]++;
         snake.insert(snake.begin(), {snake[0][0],snake[0][1]});
         snake[1][1]--;
         for(vector<G>::iterator it = growitems.begin(); it != growitems.end(); it++) {
@@ -187,8 +188,8 @@ int MoveRIGHT(int map[][45], vector<vector<int> >& snake) {
                 return KEY_RIGHT;
             }
         }
-    } else if(map[snake[0][0]][snake[0][1]] == 6) {
-        itempoint[1]++;
+    } else if(map[level][snake[0][0]][snake[0][1]] == 6) {
+        GamePoint[1]++;
         snake.pop_back();
         for(int i = 1; i < snake.size(); i++) {
             tmp = snake[i];
@@ -202,7 +203,7 @@ int MoveRIGHT(int map[][45], vector<vector<int> >& snake) {
             }
         }
     }
-    else if(map[snake[0][0]][snake[0][1]] == 4) {
+    else if(map[level][snake[0][0]][snake[0][1]] == 4) {
         return 9999;
     }
     for(int i = 1; i < snake.size(); i++) {
@@ -213,7 +214,7 @@ int MoveRIGHT(int map[][45], vector<vector<int> >& snake) {
     return KEY_RIGHT;
 }
 
-int MoveLAST(int map[][45], vector<vector<int> >& snake) {
+int MoveLAST(int map[][24][45], vector<vector<int> >& snake) {
     switch(snake[0][0] - snake[1][0]) {
         case 1:
             return MoveDOWN(map,snake);
@@ -230,47 +231,47 @@ int MoveLAST(int map[][45], vector<vector<int> >& snake) {
     return 0;
 }
 
-void GenerateGrowth(int map[][45]) {
+void GenerateGrowth(int map[][24][45]) {
     int x = rand() % 23 + 1;
     int y = rand() % 44 + 1;
-    if (map[x][y] != 0) {
+    if (map[level][x][y] != 0) {
         GenerateGrowth(map);
     }
     else {
         G item(x, y);
         growitems.push_back(item);
-        map[x][y] = 5;
+        map[level][x][y] = 5;
     }
 }
-void GeneratePoison(int map[][45]) {
+void GeneratePoison(int map[][24][45]) {
     int x = rand() % 23 + 1;
     int y = rand() % 44 + 1;
-    if (map[x][y] != 0) {
+    if (map[level][x][y] != 0) {
        GeneratePoison(map);
     }
     else {
         P item(x, y);
         poisonitems.push_back(item);
-        map[x][y] = 6;
+        map[level][x][y] = 6;
     }
 }
 
-void MakeGate(int map[][45]) {
+void MakeGate(int map[][24][45]) {
     int x1 = rand() % 23 + 1;
     int y1 = rand() % 44 + 1;
-    while( !(map[x1][y1] == 1) ) {
+    while( !(map[level][x1][y1] == 1) ) {
         x1 = rand() % 23 + 1;
         y1 = rand() % 44 + 1;
     }
     int x2 = rand() % 23 + 1;
     int y2 = rand() % 44 + 1;
-    while( !(map[x2][y2] == 1) ) {
+    while( !(map[level][x2][y2] == 1) ) {
         x2 = rand() % 23 + 1;
         y2 = rand() % 44 + 1;
     }
     Gatepair gate(x1, y1, x2, y2);
     gatelist.push_back(gate);
-    map[x1][y1] = map[x2][y2] = 7;
+    map[level][x1][y1] = map[level][x2][y2] = 7;
 }
 
 void GameOver(WINDOW* board) {
@@ -282,18 +283,6 @@ void GameOver(WINDOW* board) {
 bool signum = true;
 void sig_alarm(int sig) {
     signum = false;
-}
-
-void MapLevel(int map[][45], int level) {
-    int i = 0, x,y;
-    while(i < ((level-1) * 10)) {
-        do {
-            x = rand() % 22 + 2;
-            y = rand() % 43 + 2;
-        } while(map[x][y] == 4 || map[x][y] == 3 || map[x][y] == 1);
-        map[x][y] = 1;
-        i++;
-    }
 }
 
 int main()
@@ -361,10 +350,9 @@ int main()
     int goal_snakeSize=rand()%5 + 5;
     int goal_growthPoint=rand()%5 + 5;
     int goal_poisonPoint=rand()%3 + 5;
-    int goal_gatePoint=rand()%5 + 5;
+    int goal_gatePoint=rand()%3 ;
 
     int gateSig = 10;
-    level = 3;
     // MapLevel(M.map[level],level);
 
     for(int i = 0; i < 24; i++) {
@@ -436,17 +424,29 @@ int main()
         maxsize = maxsize <= snake.size() ? snake.size() : maxsize;
         // 보드 초기화
         string score_snakeSize = "B : " + to_string(maxsize);
-        string score_growthPoint = "+ : " + to_string(itempoint[0]);
-        string score_poisonPoint= "- : " + to_string(itempoint[1]);
-        // string score_gateCount = "G : " + to_string(usedGateCount);
+        string score_growthPoint = "+ : " + to_string(GamePoint[0]);
+        string score_poisonPoint= "- : " + to_string(GamePoint[1]);
+        string score_gatePoint = "G : " + to_string(GamePoint[2]);
         string mission_snakeSize = "B : " + to_string(goal_snakeSize);
         string mission_growthPoint = "+ : " + to_string(goal_growthPoint);
         string mission_poisonPoint= "- : " + to_string(goal_poisonPoint);
         string mission_gatePoint = "G : " + to_string(goal_gatePoint);
-        mission_snakeSize += goal_snakeSize <= maxsize ? " (v)" : " ( )";
-        mission_growthPoint += goal_growthPoint <= itempoint[0] ? " (v)" : " ( )";
-        mission_poisonPoint += goal_poisonPoint <= itempoint[1] ? " (v)" : " ( )";
-        // mission_gatePoint += goal_gatePoint <=  ? " (v)" : " ( )";   
+        if(goal_snakeSize <= maxsize){
+            mission_snakeSize += " (v)";
+            victory[0] = true;
+        } else mission_snakeSize += " ( )";
+        if(goal_growthPoint <= GamePoint[0]){
+            mission_growthPoint += " (v)";
+            victory[1] = true;
+        } else mission_growthPoint += " ( )";
+        if(goal_poisonPoint <= GamePoint[1]){
+            mission_poisonPoint += " (v)";
+            victory[2] = true;
+        } else mission_poisonPoint += " ( )";
+        if(goal_gatePoint <=  GamePoint[2]){
+            mission_gatePoint += " (v)";
+            victory[3] = true;
+        } else mission_gatePoint += " ( )";
 
         for (int i = 0; i < gatelist.size(); i++) {
             M.map[level][gatelist[i].gate1[0].first][gatelist[i].gate1[0].second] = 7;
@@ -460,10 +460,10 @@ int main()
         // 아이템 5초마다 생성
         currenttime = time(NULL);  // 현재 시간을 가져옴
         if (currenttime - tmp >= itemSig) { // 5초가 지나면
-            GenerateGrowth(M.map[level]);
-            GenerateGrowth(M.map[level]);
-            GeneratePoison(M.map[level]);
-            GeneratePoison(M.map[level]);
+            GenerateGrowth(M.map);
+            GenerateGrowth(M.map);
+            GeneratePoison(M.map);
+            GeneratePoison(M.map);
             tmp = currenttime;  // item 생성한 시간 업데이트
         }
 
@@ -489,7 +489,7 @@ int main()
         // Gate 10초마다 생성
         // Gate는 map에 7로 표시
         if (currenttime - tmp2 >= gateSig) { // 10초가 지나면
-            MakeGate(M.map[level]);
+            MakeGate(M.map);
             tmp2 = currenttime;  // gate 생성한 시간 업데이트
         }
 
@@ -556,16 +556,16 @@ int main()
             }
         }
 
-        // mvwprintw(score, 1, 5,level.c_str());
+        mvwprintw(score, 1, 5, to_string(level + 1).c_str());
         mvwprintw(score, 3, 5, score_snakeSize.c_str());
         mvwprintw(score, 4, 5, score_growthPoint.c_str());
         mvwprintw(score, 5, 5, score_poisonPoint.c_str());
-        // mvwprintw(score, 6, 5, score_gatePoint.c_str());
+        mvwprintw(score, 6, 5, score_gatePoint.c_str());
 
         mvwprintw(mission, 3, 5, mission_snakeSize.c_str());
         mvwprintw(mission, 4, 5, mission_growthPoint.c_str());
         mvwprintw(mission, 5, 5, mission_poisonPoint.c_str());
-        // mvwprintw(mission, 6, 5, mission_gatePoint.c_str());
+        mvwprintw(mission, 6, 5, mission_gatePoint.c_str());
 
         wrefresh(board);
         wrefresh(mission);
@@ -589,19 +589,19 @@ int main()
         switch (cur_dir)
         {
         case KEY_UP:
-            last_dir = MoveUP(M.map[level], snake);
+            last_dir = MoveUP(M.map, snake);
             break;
         case KEY_DOWN:
-            last_dir = MoveDOWN(M.map[level], snake);
+            last_dir = MoveDOWN(M.map, snake);
             break;
         case KEY_LEFT:
-            last_dir = MoveLEFT(M.map[level], snake);
+            last_dir = MoveLEFT(M.map, snake);
             break;
         case KEY_RIGHT:
-            last_dir = MoveRIGHT(M.map[level], snake);
+            last_dir = MoveRIGHT(M.map, snake);
             break;
         default:
-            last_dir = MoveLAST(M.map[level], snake);
+            last_dir = MoveLAST(M.map, snake);
             break;
         }
 
@@ -619,12 +619,19 @@ int main()
             gameover = true;
             break;
         }
+
+        // 승리 조건
+        if(victory[0] && victory[1] && victory[2] && victory[3]) {            
+            last_dir = 5; cur_dir = KEY_RIGHT;
+            maxsize = 3;
+            GamePoint[0] = 0; GamePoint[1] = 0; GamePoint[2] = 0;
+            victory[0] = false; victory[1] = false; victory[2] = false; victory[3] = false;
+            level++;
+        };
     }
     if(gameover == true) {
         GameOver(board);
         getch();
-    } else {
-        // 성공
     }
 
     getch(); // 사용자입력대기
